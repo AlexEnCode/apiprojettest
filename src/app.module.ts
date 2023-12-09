0d232c0794538358e3ee2984d80b7d905322285a
AlexGitCode <lineale@hotmail.com>
2023-12-09 10:45:34 +0100

    fear(library): ressource reation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { UsersModule } from './users/users.module';
+import { LibraryModule } from './library/library.module';
 
 @Module({
 
-  imports: [OrdersModule, ProductsModule, UsersModule],
+  imports: [OrdersModule, ProductsModule, UsersModule, LibraryModule],
 
   controllers: [AppController],
   providers: [AppService],
