692535c190286236ffc91932075f6853ebb371cd
AlexGitCode <lineale@hotmail.com>
2023-12-09 10:54:01 +0100

    fear(schedule): ressource reation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { UsersModule } from './users/users.module';
 import { LibraryModule } from './library/library.module';
+import { ScheduleModule } from './schedule/schedule.module';
 
 @Module({
 
-  imports: [OrdersModule, ProductsModule, UsersModule, LibraryModule],
+  imports: [OrdersModule, ProductsModule, UsersModule, LibraryModule, ScheduleModule],
 
   controllers: [AppController],
   providers: [AppService],
